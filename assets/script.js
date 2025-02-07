1d6892c90b6c7c42bb27e39de8497573948fdec3
Dillon Jayatillake <dillonmichaeljay@gmail.com>
2025-02-07 16:35:40 +0530

    Update script.js

---
+++ assets/script.js
@@ -0,0 +1,6 @@
+const apiKey = 'your_api_key_here';
+const searchButton = document.getElementById('searchButton');
+const searchInput = document.getElementById('searchInput');
+const movieResults = document.getElementById('movieResults');
+const favoriteMovies = document.getElementById('favoriteMovies');
+
